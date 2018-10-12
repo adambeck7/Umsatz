fe7a93f98a06ca3866abf548d6e06c9ff61b22c4
adambeck7 <adambeck7@gmail.com>
2018-10-11 18:10:21 -0600

    added Auth0 form

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Callback from "@/views/Callback.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
+  mode: "history", // enable history mode
   routes: [
     {
       path: "/",
@@ -19,6 +21,27 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/callback",
+      name: "callback",
+      component: Callback
     }
   ]
 });
+
+// very basic "setup" of a global guard
+router.beforeEach((to, from, next) => {
+  if (to.name == "callback") {
+    // check if "to"-route is "callback" and allow access
+    next();
+  } else if (router.app.$auth.isAuthenticated()) {
+    // if authenticated allow access
+    next();
+  } else {
+    // trigger auth0 login
+    router.app.$auth.login();
+  }
+});
+
+export default router;
